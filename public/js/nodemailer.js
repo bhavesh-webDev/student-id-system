22319856cccea64edddc7b57d37c79543fe2fb0c
bhavesh gholap <bhaveshgholap@Bhaveshs-MacBook-Air.local>
2025-09-01 19:26:19 +0530

    added error handler in nodemailer

--- public/js/nodemailer.js
+++ public/js/nodemailer.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
-import { configDotenv } from "dotenv";
-configDotenv();
+import dotenv from "dotenv";
+dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -10,12 +10,26 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (email, subject, headMessage, mainMessage) => {
-  const info = await transporter.sendMail({
-    from: "student id system <studentidsystem6@gmail.com>",
-    to: email,
-    subject: subject,
-    text: headMessage, // plain‑text body
-    html: `<b>${mainMessage}</b>`,
-  });
-  console.log("Mail Sent Successfully..");
+  try {
+    const info = await transporter.sendMail({
+      from: "student id system <studentidsystem6@gmail.com>",
+      to: email,
+      subject: subject,
+      text: headMessage, // plain‑text body
+      html: `<b>${mainMessage}</b>`,
+    });
+    console.log("Mail Sent Successfully..", info.messageId);
+  } catch (error) {
+    console.log({
+      message: "Error in send mail function...",
+      error: error.stack,
+    });
+  }
 };
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("Error in nodemailer : ", error);
+  }
+  console.log("Connected to SMTP Sucessfully : ", success);
+});
